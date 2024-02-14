c715bf0c1dced05980ca7720bfa00948d0eb2879
Ivan <ivan.ivkovic1996@gmail.com>
2024-02-14 10:14:03 +0100

    novi build

--- src/components/ProjectsComponent.js
+++ src/components/ProjectsComponent.js
@@ -31,6 +31,42 @@ const ProjectsComponent = () => {
             </div>
         </div>
         <EUnoteProject />
+        <div className="container">
+            <div className="activity-box">
+                    <h3>Aktivnosti projekta</h3>
+                    <p>Aktivnost 1: Ulaganje u materijalnu imovinu (1/2)</p>
+                    <p>Aktivnost 2: Ulaganje u materijalnu imovinu (2/2)</p>
+                    <p>Aktivnost 3: Marketing i promocija proizvoda i usluga</p>
+                    <p>Aktivnost 4: Edukacije, stručno osposobljavanje i stjecanje novih znanja zaposlenika</p>
+                    <p>Aktivnost 5: Prijava na Poziv</p>
+                    <p>Aktivnost 6: Provedba postupka nabave za potrebe projekta</p>
+                    <p>Aktivnost 7: Promicanje horizontalnih načela</p>
+                    <p>Aktivnost 8: Upravljanje projektom</p>
+                    <p>Aktivnost 9: Promidžba i vidljivost</p>
+            </div>
+        </div>
+        <div className="container">
+            <div className="activity-box">
+                    <h3>Sve aktivnosti nabave iz predmetnog projekta</h3>
+                    <p>1. planiran je postupak nabave</p>
+                    <p>2. određene su karakteristike roba i/ili usluga koje se planiraju nabavljati u postupku nabave</p>
+                    <p>3. proveden je postupak nabave</p>
+                    <p>4. odabran je isporučitelj roba i/ili usluga</p>
+                    <p>5. predmetna aktivnost je provedena u cijelosti, kako je bilo planirano, sukladno propisanim pravilima
+i projektnim prijedlogom (pridržavanje vremenskih rokova početka i završetka provedbe aktivnosti)</p>
+                    <p>6. roba i/ili usluga je od strane odabranog isporučitelja isporučena na vrijeme, do roka određenog u
+Ugovora.</p>
+                    <p>7. zaključen je zapisnik o primopredaji roba i/ili usluga</p>
+                    <p>8. izdan je račun od strane isporučitelja</p>
+                    <p>9. račun je plaćen u roku</p>
+                    <br />
+                    <p>U provedbi predmetne aktivnosti nije bilo značajnih problema.</p>
+                    <p>Ostvareni su rezultati koji su bili planirani - nabava roba i/ili usluga.</p>
+                    <p>Održane su planirane edukacije djelatnika, u količini radnih sati koje je ponudio isporučitelj.</p>
+                    <p>Nabavljena roba i/ili usluga se koristi u svakodnevnom poslovanju gospodarskog subjekta.</p>
+            </div>
+        </div>
+        <EUnoteProject />
     </div>
     );
 }
