947fa4b281f9de1ae6c5561a4edc54ce5f53cce7
Ivan <ivan.ivkovic1996@gmail.com>
2024-02-17 11:44:13 +0100

    nove izmjene po zahtjevu, novi build

--- src/components/ProjectsComponent.js
+++ src/components/ProjectsComponent.js
@@ -12,8 +12,6 @@ const ProjectsComponent = () => {
         <h1>NOVI RAZVOJ U PETRINJI SKELA PROJEKT d.o.o.</h1>
         <h2>Kod projekta: KK.08.2.1.16.0176</h2>
         <h2>Naziv poziva: Podrška razvoju poduzetništva u gradu Petrinji</h2>
-        <h2>Posredničko tijelo razine 1 (PT1): Ministarstvo regionalnoga razvoja i fondova Europske unije</h2>
-        <h2>Posredničko tijelo razine 2 (PT2): Središnja agencija za financiranje i ugovaranje programa i projekata Europske unije</h2>
         <div className="container">
             <img src={Petrinja} alt="Slika Petrinje." style={{ width: '100%', height: 'auto' }} />
             <p>Cilj ovog projekta je održiva fizička, socijalna i gospodarska regeneracija grada Petrinje, s ciljem smanjenja socijalnih nejednakosti, isključenosti i siromaštva.
@@ -24,15 +22,6 @@ const ProjectsComponent = () => {
         </div>
         <div className="container">
             <div className="box-EU">
-                <h3>Podatci o korisniku</h3>
-                <p>Prijavitelj: SKELA PROJEKT d.o.o. za građenje, usluge i trgovinu</p>
-                <p>Jedinstveni broj prijavitelja: 13198820346</p>
-                <p>Adresa: Ulica Kate Mlinarić 4, Zagreb</p>
-                <p>E-pošta: blagogudelj@gmail.com</p>
-                <p>Telefon: +385 995351688</p>
-                <p>Odgovorna osoba: BLAGO GUDELJ</p>
-            </div>
-            <div className="box-EU">
                 <h3>Vrijeme provedbe</h3>
                 <p>Početak provedbe: 18.04.2022.</p>
                 <p>Kraj provedbe: 18.11.2023.</p>
@@ -42,43 +31,71 @@ const ProjectsComponent = () => {
             <div className="box-EU">
                 <h3>Informacije o troškovima</h3>
                 <p>Ukupni prihvatljivi troškovi (EUR): 204.074,10</p>
-                <p>Ukupna bespovratna sredstva (EUR): 173.462,98 (EU sufinanciranje)</p>
+                <p>Ukupna bespovratna sredstva (EUR): 173.462,98</p>
             </div>
         </div>
         <EUnoteProject />
         <div className="container">
             <div className="activity-box">
-                    <h3>Aktivnosti projekta</h3>
-                    <p>Aktivnost 1: Ulaganje u materijalnu imovinu (1/2)</p>
-                    <p>Aktivnost 2: Ulaganje u materijalnu imovinu (2/2)</p>
-                    <p>Aktivnost 3: Marketing i promocija proizvoda i usluga</p>
-                    <p>Aktivnost 4: Edukacije, stručno osposobljavanje i stjecanje novih znanja zaposlenika</p>
-                    <p>Aktivnost 5: Prijava na Poziv</p>
-                    <p>Aktivnost 6: Provedba postupka nabave za potrebe projekta</p>
-                    <p>Aktivnost 7: Promicanje horizontalnih načela</p>
-                    <p>Aktivnost 8: Upravljanje projektom</p>
-                    <p>Aktivnost 9: Promidžba i vidljivost</p>
+                <h3>Aktivnosti projekta</h3>
+                <p>Aktivnost 1: Ulaganje u materijalnu imovinu (1/2)</p>
+                <p>Aktivnost 2: Ulaganje u materijalnu imovinu (2/2)</p>
+                <p>Aktivnost 3: Marketing i promocija proizvoda i usluga</p>
+                <p>Aktivnost 4: Edukacije, stručno osposobljavanje i stjecanje novih znanja zaposlenika</p>
+                <p>Aktivnost 5: Prijava na Poziv</p>
+                <p>Aktivnost 6: Provedba postupka nabave za potrebe projekta</p>
+                <p>Aktivnost 7: Promicanje horizontalnih načela</p>
+                <p>Aktivnost 8: Upravljanje projektom</p>
+                <p>Aktivnost 9: Promidžba i vidljivost</p>
             </div>
         </div>
         <div className="container">
             <div className="activity-box">
-                    <h3>Sve aktivnosti nabave iz predmetnog projekta</h3>
-                    <p>1. planiran je postupak nabave</p>
-                    <p>2. određene su karakteristike roba i/ili usluga koje se planiraju nabavljati u postupku nabave</p>
-                    <p>3. proveden je postupak nabave</p>
-                    <p>4. odabran je isporučitelj roba i/ili usluga</p>
-                    <p>5. predmetna aktivnost je provedena u cijelosti, kako je bilo planirano, sukladno propisanim pravilima
-i projektnim prijedlogom (pridržavanje vremenskih rokova početka i završetka provedbe aktivnosti)</p>
-                    <p>6. roba i/ili usluga je od strane odabranog isporučitelja isporučena na vrijeme, do roka određenog u
-Ugovora.</p>
-                    <p>7. zaključen je zapisnik o primopredaji roba i/ili usluga</p>
-                    <p>8. izdan je račun od strane isporučitelja</p>
-                    <p>9. račun je plaćen u roku</p>
-                    <br />
-                    <p>U provedbi predmetne aktivnosti nije bilo značajnih problema.</p>
-                    <p>Ostvareni su rezultati koji su bili planirani - nabava roba i/ili usluga.</p>
-                    <p>Održane su planirane edukacije djelatnika, u količini radnih sati koje je ponudio isporučitelj.</p>
-                    <p>Nabavljena roba i/ili usluga se koristi u svakodnevnom poslovanju gospodarskog subjekta.</p>
+                <h3>Sve aktivnosti nabave iz predmetnog projekta</h3>
+                <p>1. planiran je postupak nabave</p>
+                <p>2. određene su karakteristike roba i/ili usluga koje se planiraju nabavljati u postupku nabave</p>
+                <p>3. proveden je postupak nabave</p>
+                <p>4. odabran je isporučitelj roba i/ili usluga</p>
+                <p>5. predmetna aktivnost je provedena u cijelosti, kako je bilo planirano, sukladno propisanim pravilima i projektnim prijedlogom (pridržavanje vremenskih rokova početka i završetka provedbe aktivnosti)</p>
+                <p>6. roba i/ili usluga je od strane odabranog isporučitelja isporučena na vrijeme, do roka određenog u Ugovoru.</p>
+                <p>7. zaključen je zapisnik o primopredaji roba i/ili usluga</p>
+                <p>8. izdan je račun od strane isporučitelja</p>
+                <p>9. račun je plaćen u roku</p>
+                <br />
+                <p>U provedbi predmetne aktivnosti nije bilo značajnih problema.</p>
+                <p>Ostvareni su rezultati koji su bili planirani - nabava roba i/ili usluga.</p>
+                <p>Održane su planirane edukacije djelatnika, u količini radnih sati koje je ponudio isporučitelj.</p>
+                <p>Nabavljena roba i/ili usluga se koristi u svakodnevnom poslovanju gospodarskog subjekta.</p>
+            </div>
+        </div>
+        <EUnoteProject />
+        <div className="container">
+            <div className="activity-box">
+                <h3>Podatci o projektu</h3>
+                <p>Kod projekta: KK.08.2.1.16.0176</p>
+                <p>Posredničko tijelo razine 1 (PT1): Ministarstvo regionalnoga razvoja i fondova Europske unije</p>
+                <p>Posredničko tijelo razine 2 (PT2): Središnja agencija za financiranje i ugovaranje programa i projekata Europske unije</p>
+                <p>Početak provedbe: 18.04.2022.</p>
+                <p>Kraj provedbe: 18.11.2023.</p>
+                <p>Trajanje provedbe (mjeseci): 19</p>
+            </div>
+        </div>
+        <div className="container">
+            <div className="activity-box">
+                <h3>Podatci o korisniku</h3>
+                <p>Prijavitelj: SKELA PROJEKT d.o.o. za građenje, usluge i trgovinu</p>
+                <p>Jedinstveni broj prijavitelja: 13198820346</p>
+                <p>Adresa: Ulica Kate Mlinarić 4, Zagreb</p>
+                <p>E-pošta: blagogudelj@gmail.com</p>
+                <p>Telefon: +385 995351688</p>
+                <p>Odgovorna osoba: BLAGO GUDELJ</p>
+            </div>
+        </div>
+        <div className="container">
+            <div className="activity-box">
+                <h3>Informacije o troškovima</h3>
+                <p>Ukupni prihvatljivi troškovi (EUR): 204.074,10</p>
+                <p>Ukupna bespovratna sredstva (EUR): 173.462,98 (EU sufinanciranje)</p>
             </div>
         </div>
         <EUnoteProject />
